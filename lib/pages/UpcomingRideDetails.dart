6b86d1dbd31a99cfee2ced343afe51e332355600
ali-loubani <alil@microtech.solutions>
2021-02-09 22:34:08 +0200

    updating delete ride and getUpComingRides

--- lib/pages/UpcomingRideDetails.dart
+++ lib/pages/UpcomingRideDetails.dart
@@ -1,9 +1,15 @@
+
 import 'package:flutter/material.dart';
+import 'package:pickapp/classes/App.dart';
 import 'package:pickapp/classes/Localizations.dart';
 import 'package:pickapp/classes/Styles.dart';
 import 'package:pickapp/dataObjects/Ride.dart';
 import 'package:pickapp/pages/PassengersView.dart';
 import 'package:pickapp/pages/RideView.dart';
+import 'package:pickapp/requests/CancelRide.dart';
+import 'package:pickapp/requests/GetMyUpcomingRides.dart';
+import 'package:pickapp/requests/Request.dart';
+import 'package:pickapp/utilities/CustomToast.dart';
 import 'package:pickapp/utilities/MainAppBar.dart';
 
 class UpcomingRideDetails extends StatelessWidget {
@@ -26,7 +32,11 @@ class UpcomingRideDetails extends StatelessWidget {
                   size: Styles.largeIconSize(),
                 ),
                 tooltip: Lang.getString(context, "Delete"),
-                onPressed: (){})
+                onPressed: () async{
+                  Request<bool> request = CancelRide(ride,"hello");
+                  await request.send((result, code, message) =>
+                      response(result, code, message, context));
+                })
           ],
           title: Lang.getString(context, "Ride_Details"),
           bottom: TabBar(
@@ -49,5 +59,27 @@ class UpcomingRideDetails extends StatelessWidget {
         ),
       ),
     );
+  }
+  response(bool result, int code, String message, context) {
+    if (code !=200) {
+      CustomToast().showErrorToast(message);
+    } else {
+      Request<List<Ride>> request = GetMyUpComingRides(App.user);
+      request.send((result, code, message) =>
+          response1(result, code, message, context));
+      Navigator.pushNamedAndRemoveUntil(
+          context, "/", (Route<dynamic> route) => false);
+
+      CustomToast()
+          .showSuccessToast(Lang.getString(context, "Successfully_deleted!"));
+    }
+  }
+  response1(List<Ride> result, int code, String message, context) {
+    if (code != 200) {
+      CustomToast().showErrorToast(message);
+    } else {
+      App.user.person.upcomingRides.clear();
+      App.user.person.upcomingRides=result;
+    }
   }
 }
