c348d6912cc481a29ab98fbf79c66b2fe3fa1e1c
AdelKanso <adellkanso@gmai.com>
2021-03-14 15:32:26 +0200

    working on notf

--- lib/notifications/RateNotificationHandler.dart
+++ lib/notifications/RateNotificationHandler.dart
@@ -3,10 +3,19 @@ import 'package:pickapp/classes/App.dart';
 import 'package:pickapp/classes/Cache.dart';
 import 'package:pickapp/dataObjects/Rate.dart';
 import 'package:pickapp/dataObjects/User.dart';
+import 'package:pickapp/dataObjects/UserStatistics.dart';
+import 'package:pickapp/notifications/MainNotification.dart';
 import 'package:pickapp/notifications/NotificationsHandler.dart';
 
 class RateNotificationHandler extends NotificationHandler<Rate> {
-
+  MainNotification mainNotification;
+  RateNotificationHandler(this.mainNotification) {
+    Rate rate = Rate.fromJson((mainNotification.object as List)[0]);
+    UserStatistics stat =
+        UserStatistics.fromJson((mainNotification.object as List)[1]);
+    mainNotification.object = [rate, stat];
+    mainNotification.scheduleDate = DateTime.now().add(Duration(days: 2));
+  }
 
   @override
   void cache(Rate rate) async {
@@ -27,6 +36,4 @@ class RateNotificationHandler extends NotificationHandler<Rate> {
   void updateApp(Rate rate) {
     App.refreshProfile.value = true;
   }
-
-
 }
