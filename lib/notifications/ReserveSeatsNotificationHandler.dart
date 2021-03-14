e1b95eebfa695f80d27033accd9e890a6177cf82
hasankanso <hassankanso1095@gmail.com>
2021-03-14 16:46:45 +0100

    implement constructor of reserveNOtifcationshandler

--- lib/notifications/ReserveSeatsNotificationHandler.dart
+++ lib/notifications/ReserveSeatsNotificationHandler.dart
@@ -4,11 +4,15 @@ import 'package:pickapp/notifications/MainNotification.dart';
 import 'package:pickapp/notifications/NotificationsHandler.dart';
 
 class ReserveSeatsNotificationHandler extends NotificationHandler {
+  Ride ride;
 
+  ReserveSeatsNotificationHandler(MainNotification notification)
+      : super(notification) {
 
-  ReserveSeatsNotificationHandler(MainNotification notification) : super(notification);
-
-
+    Ride ride = Ride.fromJson(notification.object);
+    notification.object = ride;
+    this.ride = ride;
+  }
 
   @override
   void cache() {
@@ -24,6 +28,4 @@ class ReserveSeatsNotificationHandler extends NotificationHandler {
   void updateApp() {
     // TODO: implement updateApp
   }
-
-
 }
