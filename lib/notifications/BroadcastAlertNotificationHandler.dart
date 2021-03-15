43049c41e8e98270a0ba618f97e3e676c322e22d
AdelKanso <adellkanso@gmai.com>
2021-03-15 19:40:52 +0200

    create alert handler class

---
+++ lib/notifications/BroadcastAlertNotificationHandler.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/widgets.dart';
+import 'package:pickapp/dataObjects/Alert.dart';
+import 'package:pickapp/notifications/MainNotification.dart';
+import 'package:pickapp/notifications/NotificationsHandler.dart';
+
+class BroadcastAlertNotificationHandler extends NotificationHandler {
+  Alert alert;
+
+  BroadcastAlertNotificationHandler(MainNotification notification)
+      : super(notification) {
+    if (!(notification.object is Alert)) {
+      notification.object = Alert.fromJson(notification.object);
+    }
+    this.alert = notification.object;
+  }
+
+  @override
+  Future<void> cache() async {}
+
+  @override
+  void display(NavigatorState state) {
+    // TODO: implement display
+  }
+
+  @override
+  Future<void> updateApp() async {}
+}
