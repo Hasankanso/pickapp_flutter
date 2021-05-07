7c79da4f8c31261a482b96b94c3ae3b6f464e63e
AdelKanso <adellkanso@gmai.com>
2021-05-07 10:29:28 +0300

    working on cancel ride/reservation notification

--- lib/notifications/CancelReservationNotificationHandler.dart
+++ lib/notifications/CancelReservationNotificationHandler.dart
@@ -1,24 +1,38 @@
 import 'package:flutter/widgets.dart';
 import 'package:pickapp/classes/App.dart';
 import 'package:pickapp/classes/Cache.dart';
+import 'package:pickapp/dataObjects/Reservation.dart';
 import 'package:pickapp/dataObjects/Ride.dart';
 import 'package:pickapp/dataObjects/User.dart';
 import 'package:pickapp/notifications/MainNotification.dart';
 import 'package:pickapp/notifications/NotificationsHandler.dart';
 
 class CancelReservationNotificationHandler extends NotificationHandler {
-  String rideId, reason;
+  String rideId, reason, passengerId;
 
   CancelReservationNotificationHandler(MainNotification notification)
       : super(notification) {
     this.rideId = (notification.object as List)[0] as String;
-    this.reason = (notification.object as List)[1] as String;
+    this.passengerId = (notification.object as List)[1] as String;
+    this.reason = (notification.object as List)[2] as String;
   }
 
   @override
   Future<void> cache() async {
     User user = await Cache.getUser();
-    user.person.upcomingRides.remove(Ride(id: rideId));
+
+    int index = user.person.upcomingRides.indexOf(new Ride(id: rideId));
+
+    if (index < 0) return null;
+
+    Ride r = user.person.upcomingRides[index];
+
+    int passIndex = r.passengers.indexOf(new Reservation(id: passengerId));
+
+    if (passIndex < 0) return null;
+
+    user.person.upcomingRides[index].passengers.removeAt(passIndex);
+
     await Cache.setUser(user);
   }
 
--- lib/pages/RideDetails.dart
+++ lib/pages/RideDetails.dart
@@ -28,7 +28,8 @@ class RideDetails extends StatelessWidget {
   bool isEditDisabled;
   TextEditingController _reason = TextEditingController();
 
-  RideDetails(this.ride, {this.buttonText, this.onPressed, this.isEditDisabled = true});
+  RideDetails(this.ride,
+      {this.buttonText, this.onPressed, this.isEditDisabled = true});
 
   _cancelReservation(bool deleted, int code, String message, context) {
     if (code != HttpStatus.ok) {
@@ -37,7 +38,8 @@ class RideDetails extends StatelessWidget {
     } else {
       if (deleted) {
         App.deleteRideFromMyRides(ride);
-        CustomToast().showSuccessToast(Lang.getString(context, "Successfully_deleted!"));
+        CustomToast().showSuccessToast(
+            Lang.getString(context, "Successfully_canceled!"));
         Navigator.popUntil(context, (route) => route.isFirst);
       }
     }
@@ -49,9 +51,12 @@ class RideDetails extends StatelessWidget {
         _reason.text = "";
         Widget _content;
         if (ride.leavingDate.compareTo(DateTime.now()) < 0) {
-          return CustomToast().showErrorToast(Lang.getString(context, "Ride_already_started"));
+          return CustomToast()
+              .showErrorToast(Lang.getString(context, "Ride_already_started"));
         } else {
-          if (ride.leavingDate.compareTo(DateTime.now().add(Duration(hours: -48))) <= 0) {
+          if (ride.leavingDate
+                  .compareTo(DateTime.now().add(Duration(hours: -48))) <=
+              0) {
             _content = Column(
               children: [
                 TextFormField(
@@ -69,7 +74,8 @@ class RideDetails extends StatelessWidget {
                   style: Styles.valueTextStyle(),
                   validator: (value) {
                     String valid = Validation.validate(value, context);
-                    String alpha = Validation.isAlphabeticIgnoreSpaces(context, value);
+                    String alpha =
+                        Validation.isAlphabeticIgnoreSpaces(context, value);
                     String short = Validation.isShort(context, value, 15);
 
                     if (valid != null)
@@ -103,7 +109,8 @@ class RideDetails extends StatelessWidget {
                     );
                   },
                 );
-                Request<bool> request = CancelReservedSeats(ride, reason: _reason.text);
+                Request<bool> request =
+                    CancelReservedSeats(ride, reason: _reason.text);
                 request.send((v, p, s) => _cancelReservation(v, p, s, context));
               }
             },
@@ -143,7 +150,9 @@ class RideDetails extends StatelessWidget {
             tabs: [
               Tab(icon: Icon(Icons.map, size: Styles.mediumIconSize())),
               Tab(icon: Icon(Icons.person, size: Styles.mediumIconSize())),
-              Tab(icon: Icon(Icons.directions_car, size: Styles.mediumIconSize())),
+              Tab(
+                  icon: Icon(Icons.directions_car,
+                      size: Styles.mediumIconSize())),
             ],
           ),
         ),
@@ -159,7 +168,8 @@ class RideDetails extends StatelessWidget {
     );
   }
 
-  static void seatsLuggagePopUp(BuildContext context, Ride ride, Function(int, int) onPressed,
+  static void seatsLuggagePopUp(
+      BuildContext context, Ride ride, Function(int, int) onPressed,
       {Reservation reservation}) {
     if (reservation == null) {
       reservation = new Reservation(seats: 0, luggages: 0);
@@ -206,11 +216,13 @@ class RideDetails extends StatelessWidget {
         buttons: [
           DialogButton(
             child: Text(Lang.getString(context, "Confirm"),
-                style: Styles.buttonTextStyle(), overflow: TextOverflow.visible),
+                style: Styles.buttonTextStyle(),
+                overflow: TextOverflow.visible),
             color: Styles.primaryColor(),
             onPressed: () {
               if (_formKey.currentState.validate()) {
-                onPressed(seatsController.chosenNumber, luggageController.chosenNumber);
+                onPressed(seatsController.chosenNumber,
+                    luggageController.chosenNumber);
               }
             },
           ),
